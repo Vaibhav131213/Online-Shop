4cbc1d59a2167e3ece404a0f2ee04097b1eaeab0
Vaibhav131213 <98413874+Vaibhav131213@users.noreply.github.com>
2024-08-25 14:50:33 +0530

    Uploaded all files

---
+++ controllers/auth.js
@@ -0,0 +1,13 @@
+exports.getLogin = (req, res, next) => {
+  const isLoggedin = (req.get('Cookie').split(';')[3].trim().split('=')[1] === 'true')
+      res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: isLoggedin
+      });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader('Set-Cookie','loggedIn=true');
+  res.redirect('/');
+};
